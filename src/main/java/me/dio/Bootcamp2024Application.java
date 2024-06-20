bd6821d6fff0a3a940988df601f67f14eb9e90cb
Diego Nunes <diegodnunes12@gmail.com>
2024-06-20 18:28:23 -0300

    first commit

---
+++ src/main/java/me/dio/Bootcamp2024Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp2024Application.class, args);
+	}
+
+}
